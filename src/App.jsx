fb3f5daa488a2b7db79f06d902e66ff005250410
Mau_2993 <shastrimayur2993@gmail.com>
2025-03-11 23:39:48 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  const handleGenerateSpeech = () => {
+    
+  }
+
+  return (
+    <>
+      <div>
+        <input type="text" />
+        <button onClick={handleGenerateSpeech}>Generate Speech</button>
+      </div>
+    </>
+  )
+}
+
+export default App
